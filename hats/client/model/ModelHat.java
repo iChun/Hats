f8c8f90f599913a1eb6597031580086eb237ddf4
iChun <ohai.iChun@gmail.com>
2013-03-29 15:17:35 +0800

    doc parsing (1)

--- hats/client/model/ModelHat.java
+++ hats/client/model/ModelHat.java
@@ -1,12 +1,45 @@
 package hats.client.model;
 
+import java.util.ArrayList;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import net.minecraft.client.model.ModelBase;
 
 public class ModelHat extends ModelBase 
 {
 
+	public ArrayList models;
+	
+	
 	public ModelHat(Document doc)
 	{
+		models = new ArrayList();
+		
+		String[] textureSizes = doc.getElementsByTagName("TextureSize").item(0).getChildNodes().item(0).getNodeValue().split(",");
+		
+		try
+		{
+			textureWidth = Integer.parseInt(textureSizes[0]);
+			textureHeight = Integer.parseInt(textureSizes[1]);
+		}
+		catch(NumberFormatException e)
+		{
+			textureWidth = 64;
+			textureHeight = 32;
+		}
+		
+		NodeList list = doc.getElementsByTagName("Shape");
+		
+		for(int i = 0; i < list.getLength(); i++)
+		{
+			Node node = list.item(i);
+			
+			System.out.println(node.getAttributes().item(1));
+			
+		}
 		
 	}
 	
